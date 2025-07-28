95aaa32ba5e33ea05551f2bbc192868bd3326253
zasm <gatitofofito559@gmail.com>
2025-07-28 14:10:01 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app.secret_key = "WEBPOINTSSECRETKEY0213215490313213265"
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
     
